626b270dba4833f0ee6a29a94db56befe3783a33
Edgar <eandres011@ikasle.ehu.eus>
2020-05-06 15:07:10 +0200

    updated version
    - debugged some internal training problems
    - added load / save features 
    - refined modularity of the model
    - added training visualization
    - minor changes on the grade report

--- semeval_2013.py
+++ semeval_2013.py
@@ -1,6 +1,5 @@
-
 # XML treatment imports
-from os import listdir
+from os import listdir,path
 import xml.etree.ElementTree as ET
 # Deep learning imports
 import json, random, collections, re
@@ -14,6 +13,19 @@ from flair.models import SequenceTagger
 from segtok.segmenter import split_single
 # similarity imports (estimator)
 from semantic_text_similarity.models import WebBertSimilarity
+# retrieve / save models ( (de) serializtion protocol)
+import pickle
+
+'''
+Paths: Here we define the paths used by the whole script
+'''
+# this is the only line should be changed for the path indexing (maintaining the project proposed structure)
+general_path = "/media/edgar/407d4115-9ff4-45c6-9279-01b62aee0730/similarity_deep_model-master/"
+data_path = general_path + "data/"
+data_preprocessed_path = general_path + "intermedium/"
+embeddings_path = general_path + "embeddings/glove.6B.50d.txt"
+output_figures_path = general_path + "output/"
+model_path = general_path + "model/"
 
 class Preprocessor:
     '''
@@ -23,7 +35,8 @@ class Preprocessor:
     Learning required resources (word embeddings, JSON, padding ...)
 
     '''
-    def __init__(self, sequence_length, preprocess):
+    def __init__(self, sequence_length, preprocess, labelmap):
+        self.labelmap = labelmap
         self.preprocess = preprocess
         if  self.preprocess:
             self.posTagger = SequenceTagger.load('pos')
@@ -89,10 +102,7 @@ class Preprocessor:
         return indices_to_words, word_indices
 
     def load_semeval(self, snli_home):
-        LABEL_MAP = {
-            "correct": 0,
-            "incorrect": 1,
-        }
+        LABEL_MAP = self.labelmap
 
         def load_snli_data(path):
             data = []
@@ -211,18 +221,21 @@ Sentence_len = 20
 
 # ----------------------------------------------------------------
 # Lauch loaders
-preprocessor = Preprocessor(Sentence_len, preprocess=False)
-preprocessor.split_semeval(input_path="/home/edgar/Escritorio/semeval-2013-2-and-3-way/training/2way/",output_path="/home/edgar/Escritorio/semeval-2013-2-and-3-way/no_chunk/")
+label2ind =  {"correct": 0,"incorrect": 1}
+ind2label = {0:"correct",1:"incorrect"}
+preprocessor = Preprocessor(Sentence_len, preprocess=False, labelmap=label2ind)
+preprocessor.split_semeval(input_path=data_path, output_path=data_preprocessed_path)
 # load the data
-training_set, dev_set, test_set = preprocessor.load_semeval(snli_home="/home/edgar/Escritorio/semeval-2013-2-and-3-way/no_chunk/")
+training_set, dev_set, test_set = preprocessor.load_semeval(snli_home=data_preprocessed_path)
 # adequate data for the task
 indices_to_words, word_indices = preprocessor.sentences_to_padded_index_sequences([training_set, dev_set, test_set])
 # load embeddings for the task
-loaded_embeddings = preprocessor.load_embeddings(glove_home= "/home/edgar/Escritorio/semeval-2013-2-and-3-way/embeddings/glove.6B.50d.txt")
+loaded_embeddings = preprocessor.load_embeddings(glove_home=embeddings_path)
 
-class SimmilarityClassifier:
+class SimmilarityClassifier():
 
-    def __init__(self, vocab_size, sequence_length):
+    def __init__(self, vocab_size, sequence_length, labelmap):
+        self.labelmap = labelmap
         self.web_model = WebBertSimilarity(device='cpu', batch_size=10)  # defaults to GPU prediction
         # Define the hyperparameters
         self.training_epochs = 1  # How long to train for - chosen to fit within class time
@@ -230,7 +243,7 @@ class SimmilarityClassifier:
         self.display_step_freq = 11  # How often to test (in steps)
         self.dim = sequence_length  # The dimension of the hidden state of each RNN
         self.embedding_dim = 50  # The dimension of the learned word embeddings
-        self.batch_size = 64  # Somewhat arbitrary - can be tuned, but often tune for speed, not accuracy
+        self.batch_size = 16  # Somewhat arbitrary - can be tuned, but often tune for speed, not accuracy
         self.vocab_size = vocab_size  # Defined by the file reader above
         self.sequence_length = sequence_length  # Defined by the file reader above
         self.step = 1
@@ -290,6 +303,8 @@ class SimmilarityClassifier:
             #  - Attention parameters: You need to define just one variable that learns matching
             #    premise and hypothesis sequences.
             self.attn = tf.Variable(tf.random.normal([2 * self.dim, 2 * self.dim], stddev=0.1))
+            self.trainable_variables.append(self.attn)
+
         #  - Attention score is defined in eq. 8 in Luong et al. (general score)
         #  - This simplify eq.6 in Wang and Jiang for the attention score.
         #    self.score = tf.Variable(tf.random.normal([self.dim , self.dim], stddev=0.1))
@@ -297,7 +312,15 @@ class SimmilarityClassifier:
 
         # TODO: Define the paremeters for the classification layer (as in Lab5).
         self.w_cl = tf.Variable(tf.random.normal([self.dim, 2], stddev=0.1))
+        self.trainable_variables.append(self.w_cl)
         self.b_cl = tf.Variable(tf.random.normal([2], stddev=0.1))
+        self.trainable_variables.append(self.b_cl)
+
+    def package_params(self):
+        return self.trainable_variables
+
+    def setup_params(self, trainable_variables):
+        self.trainable_variables = trainable_variables
 
     # define the GRU function (Hint: check lab 5)
     def gru(self, emb, h_prev, name):
@@ -394,6 +417,9 @@ class SimmilarityClassifier:
 
             fig.tight_layout()
             plt.savefig(output_dir + title + str(i) + '_similarity.pdf')
+            plt.clf()
+            plt.cla()
+            plt.close()
 
             #normalized_pearson_correlation = tf.map_fn(lambda x: x * 0.5 + 0.5, pearson_correlation)
             #avg_correlation = tf.reduce_sum(normalized_pearson_correlation) / (len(normalized_pearson_correlation)*len(normalized_pearson_correlation[0]))
@@ -417,13 +443,17 @@ class SimmilarityClassifier:
 
         fig.tight_layout()
         plt.savefig(output_dir + title + str(i) + '_similarity_avg_line.pdf')
-        print("\n\n correlation report: \n")
-        print(" question: {}".format(examples[0]["question"]))
-        for example in examples:
-            similarity = self.web_model.predict([(example["reference_answer"], example["student_answer"])])
-            grade = self.classify([example])
-            estimated_mark = int(2*similarity)
-            print(" \n given information:\n\n reference: {} \t\t answer: {} \n score: {} \n\n results:\n\n estimated similarity: {} \t\t estimated score: {} \t\t estimated mark: {} ".format(example["reference_answer"], example["student_answer"], example["grade"], similarity, grade, estimated_mark))
+        plt.clf()
+        plt.cla()
+        plt.close()
+        with open(output_dir + title + str(i) + "_output.log", "w") as f:
+            f.write("\n\n correlation report: \n")
+            f.write(" question: {}".format(examples[0]["question"]))
+            for example in examples:
+                similarity = self.web_model.predict([(example["reference_answer"], example["student_answer"])])
+                grade = self.classify([example])
+                estimated_mark = int(2*similarity)
+                f.write(" \n given information:\n\n reference: {} \t\t answer: {} \n score: {} \n\n results:\n\n estimated similarity: {} \t\t estimated score: {} \t\t estimated mark: {} ".format(example["reference_answer"], example["student_answer"], example["grade"], similarity, self.labelmap[int(grade)], estimated_mark))
 
     # Define the model: Complete the functions
     # paper = REASONING ABOUTENTAILMENT WITHNEURALATTENTION (Rocktäschel's)
@@ -499,13 +529,11 @@ class SimmilarityClassifier:
         return logits, attn
 
     def train(self, training_data, dev_data):
-        def get_minibatch(dataset, start_index, end_index):
-            indices = range(start_index, end_index)
-            premise_vectors = np.vstack([dataset[i]['reference_answer_index_sequence'] for i in indices])
-            hypothesis_vectors = np.vstack([dataset[i]['reference_answer_index_sequence'] for i in indices])
-            labels = [dataset[i]['label'] for i in indices]
-            return premise_vectors, hypothesis_vectors, labels
         print('Training.')
+        # Training cycle
+        train_acc = []
+        dev_acc = []
+        epochs = []
         # Training cycle
         for _ in range(self.training_epochs):
             random.shuffle(training_data)
@@ -515,22 +543,24 @@ class SimmilarityClassifier:
             # Loop over all batches in epoch
             for i in range(total_batch):
                 # Assemble a minibatch of the next B examples
-                minibatch_premise_vectors, minibatch_hypothesis_vectors, minibatch_labels = get_minibatch(
+                minibatch_premise_vectors, minibatch_hypothesis_vectors, minibatch_labels = self.get_minibatch(
                     training_data, self.batch_size * i, self.batch_size * (i + 1))
 
                 # Run the optimizer to take a gradient step, and also fetch the value of the
                 # cost function for logging
                 with tf.GradientTape() as tape:
                     logits, _ , = self.model(minibatch_premise_vectors, minibatch_hypothesis_vectors)
+                    # todo implement L2 regularizers
+
                     # Define the cost function (here, the softmax exp and sum are built in)
                     total_cost = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=minibatch_labels))
 
-                # This  performs the main SGD update equation with gradient clipping
+                # This  performs the main SGD update equation with gradient clipping using Adam algorithm
                 optimizer_obj = tf.optimizers.Adam()
                 gradients = tape.gradient(total_cost, self.trainable_variables)
                 gvs = zip(gradients, self.trainable_variables)
                 capped_gvs = [(tf.clip_by_norm(grad, 5.0), var) for grad, var in gvs if grad is not None]
-                optimizer = optimizer_obj.apply_gradients(capped_gvs)
+                optimizer_obj.apply_gradients(capped_gvs)
 
                 if self.step % self.display_step_freq == 0:
                     print("Step:", self.step, "Dev acc:", self.evaluate_classifier(dev_data[0:1000]), \
@@ -542,8 +572,16 @@ class SimmilarityClassifier:
             # Display some statistics about the step
             # Evaluating only one batch worth of data -- simplifies implementation slightly
             if self.epoch % self.display_epoch_freq == 0:
-                tf.print("Epoch:", (self.epoch + 1), "Cost:", avg_cost)
+                dev_acc.append(self.evaluate_classifier(dev_data[0:1000]))
+                train_acc.append(self.evaluate_classifier(training_data[0:1000]))
+                epochs.append(self.epoch + 1)
+
+                tf.print("Epoch:", (self.epoch + 1), "Cost:", avg_cost, \
+                         "Dev acc:", dev_acc[-1], \
+                         "Train acc:", train_acc[-1])
             self.epoch += 1
+
+        return train_acc, dev_acc, epochs
 
     def classify(self, examples):
         # This classifies a list of examples
@@ -558,6 +596,13 @@ class SimmilarityClassifier:
         _ , attn_weights, = self.model(premise_vectors, hypothesis_vectors)
         # target weights matrix
         return np.reshape(attn_weights, [len(examples), self.dim, self.dim])
+
+    def get_minibatch(self, dataset, start_index, end_index):
+        indices = range(start_index, end_index)
+        premise_vectors = np.vstack([dataset[i]['reference_answer_index_sequence'] for i in indices])
+        hypothesis_vectors = np.vstack([dataset[i]['student_answer_index_sequence'] for i in indices])
+        labels = [dataset[i]['label'] for i in indices]
+        return premise_vectors, hypothesis_vectors, labels
 
 def search_examples(training_set, max_enum):
     question = training_set[0]["question"]
@@ -577,18 +622,62 @@ def search_examples(training_set, max_enum):
                 list_examples.append(example)
     return list_examples
 
-# seaarch all correct and incorrect from certain question
-examples = search_examples(training_set, 10)
-classifier = SimmilarityClassifier(len(word_indices), Sentence_len)
-print(tf.__version__) # needed tensorflow 2.x
-classifier.train(training_set, dev_set)# 10 epochs computed because is large model ...
-print("Test acc:", classifier.evaluate_classifier(test_set))
+def plot_learning_curve(par_values, train_scores, dev_scores, model_path, title="Learning Curve", xlab="", ylab="Accuracy",ylim=None):
+    """
+    Generate a simple plot of the test and training learning curve.
+
+    Parameters
+    ----------
+    par_values : list of checked values of the current parameter.
+
+    train_scores : list of scores obtained in training set (same length as par_values).
+
+    test_scores : list of scores obtained in dev set (same length as par_values)
+
+    title : string
+        Title for the chart.
+
+    ylim : tuple, shape (ymin, ymax), optional
+        Defines minimum and maximum yvalues plotted.
+    """
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel(xlab)
+    plt.ylabel(ylab)
+
+    plt.grid()
+    plt.plot(par_values, train_scores, color="r", label="Training score")
+    plt.plot(par_values, dev_scores, color="g", label="Dev score")
+
+    plt.legend(loc="best")
+    plt.savefig(model_path + 'learning_curve.pdf')
+    plt.clf()
+    plt.cla()
+    plt.close()
+
+if not path.exists(model_path+"model.pickle"):
+    print("model wasn't found, we currently generate new model")
+    classifier = SimmilarityClassifier(len(word_indices), Sentence_len, ind2label)
+    print(tf.__version__) # needed tensorflow 2.x
+    train_acc, dev_acc, epochs = classifier.train(training_set, dev_set)# 10 epochs computed because is large model ...
+    plot_learning_curve(epochs, train_acc, dev_acc, xlab="Epoch", model_path=model_path)
+    print("Test acc:", classifier.evaluate_classifier(test_set))
+    # save model
+    pickle_out = open(model_path+"model.pickle","wb")
+    pickle.dump(classifier.package_params(), pickle_out)
+else:
+    print("model was found, we currently loading the model")
+    # load model
+    pickle_in = open(model_path+"model.pickle", "rb")
+    classifier = SimmilarityClassifier(len(word_indices), Sentence_len, ind2label)
+    classifier.setup_params(pickle.load(pickle_in))
+    print("Test acc:", classifier.evaluate_classifier(test_set))
+
+# expected Test acc: 0.782608695652174
 
 # here we generate full understandable report (show the internal information) and check
-classifier.estimate_grade(search_examples(training_set[350:400], 10), "/home/edgar/Escritorio/semeval-2013-2-and-3-way/figures/", "1_exp")
-classifier.estimate_grade(search_examples(training_set[150:200], 10), "/home/edgar/Escritorio/semeval-2013-2-and-3-way/figures/", "2_exp")
-classifier.estimate_grade(search_examples(training_set[250:300], 10), "/home/edgar/Escritorio/semeval-2013-2-and-3-way/figures/", "3_exp")
-classifier.estimate_grade(search_examples(training_set[450:500], 10), "/home/edgar/Escritorio/semeval-2013-2-and-3-way/figures/", "4_exp")
-classifier.estimate_grade(search_examples(training_set[700:750], 10), "/home/edgar/Escritorio/semeval-2013-2-and-3-way/figures/", "5_exp")
-classifier.estimate_grade(search_examples(dev_set, 10), "/home/edgar/Escritorio/semeval-2013-2-and-3-way/figures/", "6_exp")
-classifier.estimate_grade(search_examples(test_set, 10), "/home/edgar/Escritorio/semeval-2013-2-and-3-way/figures/", "7_exp")
+classifier.estimate_grade(search_examples(test_set[:50], 10), output_figures_path, "1_exp")
+classifier.estimate_grade(search_examples(test_set[50:100], 10), output_figures_path, "2_exp")
+classifier.estimate_grade(search_examples(test_set[100:150], 10), output_figures_path, "3_exp")
